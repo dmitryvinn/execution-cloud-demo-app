6f20d37a2c382808acd112ff993355238d899cc5
Dmitry Vinnik <dmitryvinn@users.noreply.github.com>
2023-06-21 16:11:31 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3 class="docs-title" id="docs-titl">Documentation &rarr;</h3>
+            <h3 class="docs-title" id="docs-title">Documentation &rarr;</h3>
             <p id="docs-desc">Find in-depth information about Next.js features and API.</p>
           </a>
 
